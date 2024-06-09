fb95ef039b04c82fb10601b569bf90eb9149919e
Bruninhu <brunoacristo13@gmail.com>
2024-06-09 14:22:52 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import style from "./styles/page.module.css"
-import Main from "./components/main";
+import Main from "./components/Main";
 
 
 export default async function Home() {
