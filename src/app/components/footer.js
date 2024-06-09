75d725ae5958438c3d4c33a34a63211b4bc44f13
Bruninhu <brunoacristo13@gmail.com>
2024-06-09 12:39:29 -0300

    Update footer.js

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,4 +1,26 @@
 import Image from "next/image"
-import style from "./footer.module.css"
+import style from "../styles/footer.module.css"
 
-export default function Footer
+export default function Footer(){
+    return(
+        <footer className={styles.rodape}>   
+        <ul>
+            <li>
+                <a href="https://instagram.com">
+                <p>Instagram</p>
+                </a>
+            </li>
+            <li>
+                <a href="https://tiktok.com">
+                <p>Tik Tok</p>
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com">
+                <p>Twitter</p>
+                </a>
+            </li>
+        </ul>
+        </footer>
+    )
+}
