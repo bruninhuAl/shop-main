b9e40e9c5eb828fa473d55632291e6a0cb877c20
bruninhuAl <brunoacristo13@gmail.com>
2024-06-28 17:21:08 -0400

    commit 28/06

--- src/app/components/main.js
+++ src/app/components/main.js
@@ -1,20 +1,27 @@
 'use client'
 import { useEffect, useState } from "react";
-import style from "../styles/main.module.css"
+import style from "../styles/main.module.css";
 import Spinner from "./spinner";
+import ErrorFetch from "./ErrorFetch";
 
 export default function Main() {
 
   const [listProducts, setProducts] = useState([]);
   const [listComplete, setComplete] = useState([]);
   const [textSearch, setTextSearch] = useState([]);
+  const [isError, setIsError] = useState(false);
 
   useEffect( ()=> {   
     const getProduct = async() => {
+      try {
       const response = await fetch("https://fakestoreapi.com/products")
       const data = await response.json();
       setProducts(data);
       setComplete(data);
+      }
+      catch{
+        setIsError(true);
+      }
     }
     getProduct();
   },[]);
@@ -55,11 +62,16 @@ export default function Main() {
         return
       }
       const newList = listProducts.filter(() =>
-        produto.title.topUpperCase().includes(text.topUpperCase())
+        produto.title.topUpperCase().Spinnertrim().includes(textSearch.topUpperCase())
       );
+      listComplete(newList);
     }
 
-    if( listProducts[0] == null){
+    if(isError == true){
+      return <ErrorFetch />
+    }
+
+    if( listComplete[0] == null){
       return <Spinner/>
     }
 
--- src/app/components/spinner.js
+++ src/app/components/spinner.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import style from "../styles/main.module.css"
 
 export default function Spinner(){
     return(
+        <div classname={StyleSheet.carregando}>
         <Image width={100} height={100} alt="" src={"/loading.svg"}/>
+        </div>
     )
 }
